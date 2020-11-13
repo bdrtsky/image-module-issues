f4ac0753514296bc4c6710cfaa135cf993b505c8
Sergey Bedritsky <sergey.bedritsky@gmail.com>
2020-11-13 14:21:07 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtImage sizes="100,300:300,500:500,1000:1000" responsive src="cloudinary+4/3:/vue-telemetry/4d02deda597adab13fac9341c22458aa.jpg" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
